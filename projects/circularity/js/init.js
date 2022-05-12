4af3ee115b80e0d37d44e3a0062ccbdb25b09efe
JoeDaIV <Joedaiv60@gmail.com>
2022-05-12 00:25:07 +0000

    I am finished and I want cry

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -23,23 +23,21 @@ var init = function (window) {
         // TODO 1: Declare our variables //
         var circle;
         var circles = [];
-        var rightEdge = circle.x + circle.radius;
+       // var rightEdge = circle.x + circle.radius;
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 5);
             view.addChild(circle);
             circles.push(circle);
         }
 
-        
+
         // TODO 3 / 8 : Call the drawCircle() function
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
-        physikz.addRandomVelocity(circle, canvas, 10, 10);
+        for(var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -71,59 +69,58 @@ var init = function (window) {
 
         }
 
+
+
+        /* 
+        This Function should check the position of a circle that is passed to the 
+        Function. If that circle drifts off the screen, this Function should move
+        it to the opposite side of the screen.
+        */
+        game.checkCirclePosition = function (circle) {
+
+
+            // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+            if (circle.x > canvas.width) {
+                circle.x = 0;
+            }
+            // if the circle has gone past the left of side the screen then place it on the right
+            if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+            // if the cirlce has gone too far down the screen then place it at the top
+            if (circle.y > canvas.height) {
+                circle.y = 0;
+            }
+            // if the circle has gone too far up the screen then place it at the bottom
+            if (circle.y < 0) {
+                circle.y = canvas.height;
+            }
+
+
+
+            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
+
+
+
+
+            // YOUR TODO 7 CODE ENDS HERE //////////////////////////
+        }
+
+        /////////////////////////////////////////////////////////////
+        // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
+        /////////////////////////////////////////////////////////////
+
+        view.addChild(fps);
+        app.addUpdateable(fps);
+
+        game.circle = circle;
+        game.circles = circles;
+        game.drawCircle = drawCircle;
+        game.update = update;
+
+        app.addUpdateable(window.opspark.game);
     }
-
-    /* 
-    This Function should check the position of a circle that is passed to the 
-    Function. If that circle drifts off the screen, this Function should move
-    it to the opposite side of the screen.
-    */
-    game.checkCirclePosition = function (circle) {
-
-
-        // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-        if (circle.x > canvas.width) {
-            circle.x = 0;
-        }
-        // if the circle has gone past the left of side the screen then place it on the right
-        if (circle.x < 0) {
-            circle.x = canvas.width;
-        }
-        // if the cirlce has gone too far down the screen then place it at the top
-        if (circle.y > canvas.height) {
-            circle.y = 0;
-        }
-        // if the circle has gone too far up the screen then place it at the bottom
-        if (circle.y < 0) {
-            circle.y = canvas.height;
-        }
-    }
-
-
-    // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-    for (var drawCircle = 0; drawCircle < 100; drawCircle++) {
-
-    }
-
-
-
-    // YOUR TODO 7 CODE ENDS HERE //////////////////////////
 }
-
-/////////////////////////////////////////////////////////////
-// --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
-/////////////////////////////////////////////////////////////
-
-view.addChild(fps);
-app.addUpdateable(fps);
-
-game.circle = circle;
-game.circles = circles;
-game.drawCircle = drawCircle;
-game.update = update;
-
-app.addUpdateable(window.opspark.game);
-
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if ((typeof process !== 'undefined') &&
