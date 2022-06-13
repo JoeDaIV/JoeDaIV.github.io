1b526820099f5dab4fe2aae1fa62c4bab9cb7e8f
JoeDaIV <Joedaiv60@gmail.com>
2022-06-13 19:41:10 +0000

    Im having some problems

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,6 +1,6 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads and is where you should call your functions.
-$(document).ready(function(){
+$(document).ready(function () {
     const $display = $('#display');
 
     // Multiple TODOs: Call your apply function(s) here
@@ -17,8 +17,22 @@ $(document).ready(function(){
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
-
+function applyFilter(filterFunction) {
+    for (var r = 0; r < image.length; r++) {
+        var row = image[r];
+        for(var c = 0;c < row.length;c++){
+            var value = image[r][c];
+            var rgbString = value;
+            var rgbNumbers = rgbStringToArray(rgbString);
+            filterFunction(rgbNumbers);
+            var rgbString = rgbArrayToString(rgbNumbers);
+            var value = rgbString;
+        }
+    }
+}
+function applyAndRender() {
+    applyFilter(reddify);
+}
 // TODO 7: Create the applyFilterNoBackground function
 
 
@@ -26,7 +40,10 @@ $(document).ready(function(){
 
 
 // TODO 3: Create reddify function
-
+function reddify(rArr){
+    rArr[RED] = 200;
+    
+}
 
 // TODO 6: Create more filter functions
 
