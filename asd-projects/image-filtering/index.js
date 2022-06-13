797e00fc8e76f6defd80555c6709854eb34bd551
JoeDaIV <Joedaiv60@gmail.com>
2022-06-13 21:19:20 +0000

    Finished

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     const $display = $('#display');
 
     // Multiple TODOs: Call your apply function(s) here
-
+    //  applyFilter(reddify);
+    applyFilterNoBackground(reddify);
 
 
 
@@ -20,32 +21,57 @@ $(document).ready(function () {
 function applyFilter(filterFunction) {
     for (var r = 0; r < image.length; r++) {
         var row = image[r];
-        for(var c = 0;c < row.length;c++){
-            var value = image[r][c];
-            var rgbString = value;
-            var rgbNumbers = rgbStringToArray(rgbString);
-            filterFunction(rgbNumbers);
-            var rgbString = rgbArrayToString(rgbNumbers);
-            var value = rgbString;
+        for (var c = 0; c < row.length; c++) {
+            var value = image[r][c]; // storing the image array in a variable
+            var rgbString = value; // storing that variable in another variable to use 
+            var rgbNumbers = rgbStringToArray(rgbString); // making a new array and saving it in rgbNumbers
+            filterFunction(rgbNumbers); // changing/filtering the red collor in the image
+            rgbString = rgbArrayToString(rgbNumbers); // assigning a new array back to rgbString
+            image[r][c] = rgbString;
         }
     }
 }
-function applyAndRender() {
-    applyFilter(reddify);
-}
-// TODO 7: Create the applyFilterNoBackground function
 
+// TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+    var backgroundColor = image[0][0]; // gets the background color
+    for (var r = 0; r < image.length; r++) {
+        var row = image[r];
+        for (var c = 0; c < row.length; c++) {
+            var value = image[r][c];
+            var rgbString = value; // storing that variable in another variable to use 
+            if (backgroundColor !== value) { // makes it so we do not change the background color
+                var rgbNumbers = rgbStringToArray(rgbString); // making a new array and saving it in rgbNumbers
+                filterFunction(rgbNumbers); // changing/filtering the red collor in the image
+                rgbString = rgbArrayToString(rgbNumbers); // assigning a new array back to rgbString
+                image[r][c] = rgbString;
+            }
+        }
+    }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(a) {
+    var low = Math.max(a, 0);
+    var high = Math.min(low, 255);
+    return high;
+}
 
 // TODO 3: Create reddify function
-function reddify(rArr){
+function reddify(rArr) {
     rArr[RED] = 200;
-    
+
 }
 
 // TODO 6: Create more filter functions
-
+function decreaseBlue(dbArr) {
+    dbArr[BLUE] = keepInBounds(dbArr[BLUE] - 50);
+}
+function increaseGreenByBlue(gArr) {
+    gArr[GREEN] = keepInBounds(gArr[GREEN] + gArr[BLUE]);
+}
 
 // CHALLENGE code goes below here
+
+
+
