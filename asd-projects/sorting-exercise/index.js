87efafb7282c3bfa3805ebd525756fb566a8a210
JoeDaIV <Joedaiv60@gmail.com>
2022-06-16 21:06:06 +0000

    new project

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,15 +14,45 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array) {
+    for (var i = 0; i < array.length - 1; i++) {
+        var iArr = array[i].value;
+        console.log(iArr);
+        for (var j = array.length - 1; j > i + 1; j--) {
+            console.log({i, j});
+            if (array[j].value < array[j - 1].value) {
+                swap(array, j, j - 1);
+                updateCounter(bubbleSort);
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
-
+async function quickSort(array, left, right){
+    if(array.length < 1){
+        return;
+    }
+    var index = await partition(array, left, right);
+    if(left < index - 1){
+      await quickSort(array, left, index - 1);
+    }
+    if(right > index){
+        await quickSort(array, index, right);
+    }
+}
 
 // TODOs 4 & 5: Implement partition
-
+function partition(){}
 
 // TODO 1: Implement swap
+async function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    drawSwap(array, i, j);
+}
 
 
 ///////////////////////////////////////////////////////////////////////
@@ -32,12 +62,12 @@ The CSS ids you will work with are:
 //////////////////////////// HELPER FUNCTIONS /////////////////////////
 
 // this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
+function sleep() {
     return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
 }
 
 // This function draws the swap on the screen
-function drawSwap(array, i, j){
+function drawSwap(array, i, j) {
     let element1 = array[i];
     let element2 = array[j];
 
@@ -48,6 +78,6 @@ function drawSwap(array, i, j){
 }
 
 // This function updates the specified counter
-function updateCounter(counter){
+function updateCounter(counter) {
     $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
 }
