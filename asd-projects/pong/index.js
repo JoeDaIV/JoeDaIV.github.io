71f25c1ff1cc23ea437a4561c0d45601834c5b43
JoeDaIV <Joedaiv60@gmail.com>
2022-06-21 21:01:02 +0000

    worked on pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,44 +12,70 @@ function runProgram() {
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
   // Game Item Objects
-  var x = parseFloat($("#leftPaddle").css("left"));
-  var y = parseFloat($("#leftPaddle").css("top"));
-  var width = $("#leftPaddle").width();
-  var height = $("#leftPaddle").height();
-  var speedY = 0;
 
-  var x = parseFloat($("#rightPaddle").css("left"));
-  var y = parseFloat($("#rightPaddle").css("top"));
-  var width = $("#rightPaddle").width();
-  var height = $("#rightPaddle").height(); 
-  var speedY = 0;
+  function GameObject($id) {
+    var obj = {};
+    obj.id = $id;
+    obj.x = parseFloat($($id).css("left"));
+    obj.y = parseFloat($($id).css("top"));
+    obj.width = $($id).width();
+    obj.height = $($id).height();
+    obj.color = $($id).css("background-color");
+    obj.speedY = 0;
+    obj.speedX = 0;
+    return obj;
+  }
 
-  var x = parseFloat($("#ball").css("left"));
-  var y = parseFloat($("#ball").css("top"));
-  var width = $("#ball").width();
-  var height = $("#ball").height();
-  var speedY = 0;
-  var speedX = 0;
-
-  function gameItems(x, y, width, height){
+  var ball = GameObject("#ball", "220px", "220px", "20px", "20px", "red", 10, 10);
+  var leftPaddle = GameObject("#leftPaddle", "0px", "220px", "20px", "80px", "mid-night blue", 0);
+  var rightPaddle = GameObject("#rightPaddle", "440px", "220px", "20px", "80px", "scarlet", 0);
 
 
+  var keyRight = {
+    UP: 38,
+    DOWN: 40
+  }
+  var keyLeft = {
+    W: 87,
+    S: 83
   }
 
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on("keyup", handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+
+  function handleKeyDown(event) {
+    if(event.which === keyLeft.W){
+      leftPaddle.speedY = leftPaddle.y + -12;
+    console.log(52);
+    }
+    if(event.which === keyLeft.S){
+      leftPaddle.speedY += 12;
+    }
+    if(event.which === keyRight.UP){
+      rightPaddle.speedY += -12;
+    }
+    if(event.which === keyRight.DOWN){
+      rightPaddle.speedY += 12;
+      console.log(25);
+    }
+  }
+  
+  function handleKeyUp(){}
 
   /* 
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    GameObject();
 
 
   }
