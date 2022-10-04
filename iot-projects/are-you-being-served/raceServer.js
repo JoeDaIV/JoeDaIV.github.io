4a57654546ea659871fcba943b8ce2ee4420649c
JoeDaIV <joedaiv60@gmail.com>
2022-10-04 18:40:42 -0500

    the race is on

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const async = require("async");
 
-const port = 8686;
+const port = 1061;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -9,17 +9,31 @@ http.createServer(function(req, res) {
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 7: Get the start time for the race
-    
+    let d = new Date();
+    let startTime = d.getTime();
 
     // TODO 12: Make the whole thing parallel
-    async.series( 
+    async.parallel( 
         // TODO 9: Supply an array of functions
-        [
-            
+        [function(callback){wrapper(callback)},
+            function(callback){wrapper(callback)},
+            function(callback){wrapper(callback)},
+            function(callback){wrapper(callback)},
         ],
         function (error, results) {
             // TODO 10: add a callback function to the end of the async call to tally the results 
-
+            res.write("Results:\n");
+            var victoryOrder = sortTogether(racers, results);
+            for (var i = 0;i < victoryOrder.length;i++){
+                res.write("Green Ambler:\n"); 
+                res.write("Catalack:\n"); 
+                res.write("Steel Runner:\n");
+                res.write("G.I. Jogger:\n");
+                d = new Date();
+                var endTime = d.getTime();
+            }
+            var duration = startTime - endTime;
+            res.end(duration + "\n");
         }
     );
     
@@ -27,6 +41,11 @@ http.createServer(function(req, res) {
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback){
+    setTimeout(function(){
+        var d = new Date();
+        callback(null, d.getTime()) ,
+     Math.random()*1000
+    })
 
 }
 
