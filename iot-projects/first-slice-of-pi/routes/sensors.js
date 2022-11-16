c7028e28962c3adc394342bfc8008924d48037a1
JoeDaIV <joedaiv60@gmail.com>
2022-11-15 18:29:19 -0600

    todo 3

--- iot-projects/first-slice-of-pi/routes/sensors.js
+++ iot-projects/first-slice-of-pi/routes/sensors.js
@@ -1,7 +1,22 @@
-const express = require('express'),
-	router = express.Router(),
-	resources = require('./../resources/model');
+const express = require("express"),
+  router = express.Router(),
+  resources = require("./../resources/model");
+router.route("/").get(function (req, res, next) {
+  res.send(resources.pi.sensors);
+});
 
+router.route("/dht").get(function (req, res, next) {
+  res.send(resources.pi.sensors.dht);
+});
+router.route("/").get(function (req, res, next) {
+  res.send(resources.pi.sensors.dht.humidity);
+});
 
+router.route("/dht").get(function (req, res, next) {
+  res.send(resources.pi.sensors.dht.temperature);
+});
+router.route("/").get(function (req, res, next) {
+  res.send(resources.pi.sensors.pir);
+});
 
 module.exports = router;
--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,16 +1,17 @@
-const express = require('express'),
-	cors = require('cors');
-	var app = express();
+const express = require("express"),
+  cors = require("cors");
+var sensorRoutes = require("./../routes/sensors");
+var app = express();
 
 app.use(cors());
 
-app.get('/', function(req, res){
-	res.send('You have accessed the root');
- });
- app.get('/pi', function(req, res){
-	res.send('You have entered the pi');
- });
+app.use("/pi/sensors", sensorRoutes);
+app.get("/", function (req, res) {
+  res.send("You have accessed the root");
+});
+app.get("/pi", function (req, res) {
+  res.send("You have entered the pi");
+});
 
 module.exports = app;
 // I have looked through all files
-
