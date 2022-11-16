e6c9dfb38e82bf3f1d43e0ca35d1e1e0b018ae34
JoeDaIV <joedaiv60@gmail.com>
2022-11-15 18:56:09 -0600

    todo 3

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -2,9 +2,11 @@ const express = require("express"),
   cors = require("cors");
 var sensorRoutes = require("./../routes/sensors");
 var app = express();
+var actuatorRoutes = require('./../routes/actuators');
 
 app.use(cors());
 
+app.use("/routes/actuators", actuatorRoutes);
 app.use("/pi/sensors", sensorRoutes);
 app.get("/", function (req, res) {
   res.send("You have accessed the root");
@@ -12,6 +14,7 @@ app.get("/", function (req, res) {
 app.get("/pi", function (req, res) {
   res.send("You have entered the pi");
 });
+
 
 module.exports = app;
 // I have looked through all files
