eb2d3cb971ab5f4aa89dcfa534d7e938962a70a1
JoeDaIV <joedaiv60@gmail.com>
2022-11-29 18:45:40 -0600

    second pi

--- iot-projects/first-slice-of-pi/routes/actuators.js
+++ iot-projects/first-slice-of-pi/routes/actuators.js
@@ -2,13 +2,13 @@ const express = require("express"),
   router = express.Router(),
   resources = require("./../resources/model");
 router.route("/").get(function (req, res, next) {
-  res.send(resources.pi.sensors);
+  req.result = resources.pi.sensors;
 });
 router.route("/leds").get(function (req, res, next) {
-  res.send(resources.pi.sensors);
+  req.result = resources.pi.sensors;
 });
 router.route("/leds/:id").get(function (req, res, next) {
-  res.send(resources.pi.actuators.leds[req.params.id]);
+  req.result = resources.pi.actuators.leds[req.params.id];
 });
 
 module.exports = router;
--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,11 +1,14 @@
 const express = require("express"),
+mid = require('./../middleware/converter'),
+bodyParser = require('body-parser'),
   cors = require("cors");
 var sensorRoutes = require("./../routes/sensors");
 var app = express();
+app.use(converter());
 var actuatorRoutes = require('./../routes/actuators');
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use("/pi/actuators", actuatorRoutes);
 app.use("/pi/sensors", sensorRoutes);
 app.get("/", function (req, res) {
