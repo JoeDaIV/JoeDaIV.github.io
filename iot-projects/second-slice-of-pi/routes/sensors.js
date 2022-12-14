75cfaccd9ef75473e3d72b91b1562639da7743d0
dkogler <danielk373@gmail.com>
2022-12-13 18:07:18 -0600

    adding complete second slice

--- iot-projects/second-slice-of-pi/routes/sensors.js
+++ iot-projects/second-slice-of-pi/routes/sensors.js
@@ -1,22 +1,31 @@
-const express = require("express"),
-  router = express.Router(),
-  resources = require("./../resources/model");
-router.route("/").get(function (req, res, next) {
-  res.send(resources.pi.sensors);
+var express = require('express'),
+	router = express.Router(),
+	resources = require('./../resources/model');
+
+router.route('/').get(function (req, res, next) {
+	req.result = resources.pi.sensors;
+	next();
 });
 
-router.route("/dht").get(function (req, res, next) {
-  res.send(resources.pi.sensors.dht);
-});
-router.route("/dht/humidity").get(function (req, res, next) {
-  res.send(resources.pi.sensors.dht.humidity);
+router.route('/pir').get(function (req, res, next) {
+	req.result = resources.pi.sensors.pir;
+	next();
 });
 
-router.route("/dht/temperature").get(function (req, res, next) {
-  res.send(resources.pi.sensors.dht.temperature);
+router.route('/dht').get(function (req, res, next) {
+	req.result = resources.pi.sensors.dht;
+	next();
 });
-router.route("/pir").get(function (req, res, next) {
-  res.send(resources.pi.sensors.pir);
+
+router.route('/dht/temperature').get(function (req, res, next) {
+	req.result = resources.pi.sensors.dht.temperature;
+	next();
+});
+
+router.route('/dht/humidity').get(function (req, res, next) {
+	req.result = resources.pi.sensors.dht.humidity;
+	next();
 });
 
 module.exports = router;
+
