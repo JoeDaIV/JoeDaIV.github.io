7d7da1cd79c6d5579111c56bebd8aba5581d92b3
JoeDaIV <Joedaiv60@gmail.com>
2023-02-15 01:58:45 +0000

    Calc-Distance

--- video-game-course/calculate-distance/index.js
+++ video-game-course/calculate-distance/index.js
@@ -1,9 +1,9 @@
 // TODO 4: Add a parameter for your gaming library in the index.js module:
-(function(window, createjs, opspark) {
+(function (window, createjs, opspark, mugiwara) {
   // OUR MODULE CODE GOES BELOW HERE //
 
   console.log('index.js initialized!');
-  
+
   /*
    * In this project, we'll use our getDistance() method to 
    * determine if the mouse is intersecting with a Shape.
@@ -21,8 +21,8 @@
     engine = opspark.V6().activateResize(),
     canvas = engine.getCanvas(),
     stage = engine.getStage(),
-    
-    
+
+
     radius = 25, // the radius of our two circles
     shapeUp = new createjs.Shape(), // the up state: the mouse is NOT intersecting
     shapeOver = new createjs.Shape(); // the over state: the mouse IS intersecting
@@ -45,40 +45,42 @@
   const
     textfield = new createjs.Text('Distance: ', "20px Arial", "#BBB"),
     textBounds = textfield.getBounds();
-    
+
   textfield.x = (canvas.width - textBounds.width) / 2;
   textfield.y = canvas.height / 2 + 50;
-  
+
   stage.addChild(shapeUp, shapeOver, textfield);
-  
+
   // The update() method is called 60 times a second //
   function update(event) {
     /*
      * TODO 5: use getDistance to calculate the distance between shapeUp and 
      * the mouse. Store the result in a variable called distance:
      */
-    
-    
-    
+    var a = shapeOver.x - shapeUp.x;
+    var b = shapeOver.y - shapeUp.y;
+    var distance = getDistance(a*a + b*b);
+
+
     /*
      * TODO 6: Check if the mouse is within the area of shapeUp, and set the 
      * alpha property of shapeUp accordingly:
      */
-    
-    
+
+
     /*
      * Update the textfield with the current distance between the mouse and 
      *the edge of the shapeUp
      */
     updateText(textfield, `Distance: ${Math.round(distance)}px`);
   }
-  
-  
+
+
   // this method updates the text on a textfield, then re-centers the textfield //
   function updateText(textfield, text) {
     textfield.text = text;
     const textBounds = textfield.getBounds();
-    
+
     // re-center the text each time it changes //
     textfield.x = (canvas.width - textBounds.width) / 2;
     textfield.y = canvas.height / 2 + 50;
@@ -98,6 +100,6 @@
     .activateTick();
 
   // OUR MODULE CODE GOES ABOVE HERE //
-  
-// TODO 3: Pass your gaming library into the index.js module:
-}(window, window.createjs, window.opspark));
+
+  // TODO 3: Pass your gaming library into the index.js module:
+}(window, window.createjs, window.opspark, window.mugiwara));
