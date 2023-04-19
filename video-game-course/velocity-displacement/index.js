12c12b7f730140f06d29f400834b2b6ba31762b5
JoeDaIV <Joedaiv60@gmail.com>
2023-04-19 00:57:20 +0000

    Finished Velocity displacement

--- video-game-course/velocity-displacement/index.js
+++ video-game-course/velocity-displacement/index.js
@@ -37,7 +37,7 @@
      * is available to you in this scope as, "this".
      * 2. What are the x and y forces acting on our ship?
      */
-    phyz.updateVelocity(this)
+     phyz.updateVelocity(this, this.propulsion, this.propulsion)
     
     
     // also check if the ship needs to rebound off a boundary //
@@ -79,7 +79,19 @@
   // listen for user releasing keys //
   document.onkeyup = function(event) {
     // TODO 13: How do we stop the application of forces?
-    
+  if(event.key === "ArrowUp"){
+    ship.rotationalVelocity = 0;
+    ship.propulsion = 0;
+  }
+  if(event.key === "ArrowLeft"){
+    ship.rotationalVelocity = 0;
+    ship.propulsion = 0;
+  }
+  if(event.key === "ArrowRight"){
+    ship.rotationalVelocity = 0;
+    ship.propulsion = 0;
+  }
+
   };
   
   function reboundCircularAssetInArea(body, area) {
@@ -95,10 +107,11 @@
       // we've struck the right side of the area //
       body.x = right - radius;
       body.velocityX *= -1;
-    } else if ( /* TODO 9: Check if body's hit left side */ false ) {
+    } else if ( body.x + radius < left) {
       // we've struck the left side of the area //
       // TODO 10: Code the reaction to hitting the left side
-      
+      body.x = left + radius;
+      body.velocityX *= -1;
     }
 
     // check for hit on top or bottom //
@@ -106,10 +119,11 @@
       // we've struck the right side of the area //
       body.y = top + radius;
       body.velocityY *= -1;
-    } else if ( /* TODO 11: Check if body's hit bottom */ false ) {
+    } else if ( body.y - radius > bottom) {
       // we've struck the bottom of the area //
       // TODO 12: Code the reaction to hitting the bottom
-      
+      body.y = bottom + radius;
+      body.velocityY *= -1;
     }
   }
   
